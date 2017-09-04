ffae31a04f75fea7bf44b91831e38cdd6a46cfcb
davidruffner <davidruffner@gmail.com>
2017-09-04 16:03:55 -0400

    Do not need explicity cors request.

--- script.js
+++ script.js
@@ -1,62 +1,10 @@
-
-
-//Run Sample
-
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
-
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-}
-
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // This is a sample server that supports CORS.
-  var url = 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html';
-
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-}
-
-makeCorsRequest();
-
 /*
  * https://stackoverflow.com/questions/247483/http-get-request-in-javascript
  */
 console.log('AAAAAAAA')
 var xhr = new XMLHttpRequest();
-var wordListURL = "https://github.com/davidruffner/scrabble-word-finder/raw/master/resources/enable1-wwf-v4.0-wordlist.txt"
+var wordListURL = 'https://davidruffner.github.io/scrabble-word-finder/resources/enable1-wwf-v4.0-wordlist.txt';
+
 xhr.open('GET', wordListURL, true);
 xhr.send();
 
