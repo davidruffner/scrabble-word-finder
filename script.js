00f3543f3faa214339121f8e25782a5dd676075e
davidruffner <davidruffner@gmail.com>
2017-09-04 17:06:39 -0400

    Add regex pattern and check functions.

--- script.js
+++ script.js
@@ -36,21 +36,51 @@ function get(url) {
 var wordListURL = 'https://davidruffner.github.io/scrabble-word-finder/resources/enable1-wwf-v4.0-wordlist.txt';
 var wordListRequest = get(wordListURL)
 
-function myFunction() {
+function findWords() {
     var letters = document.getElementById("letters").value;
     var startsWith = document.getElementById("startsWith").value;
     var endsWith = document.getElementById("endsWith").value;
     var gapLength = document.getElementById("gapLength").value;
 
+    /* Make sure that word list has been loaded */
     wordListRequest.then(function(response) {
-      console.log("Success!", response);
+      var words = response.split(/\r?\n/);  // Split on newlines
+      console.log("Success1!", words[0]);
+      var pattern = createPattern(startsWith, endsWith, gapLength);
+
+      console.log('A match? ' + checkWord(pattern, words[0]));
       document.getElementById("demo").innerHTML = letters + startsWith + endsWith + gapLength;
     }, function(error) {
       console.error("Failed!", error);
-    })
+    });
+}
 
+/**
+  * Finds regex pattern to pick out words that start with and
+  * end with certain letters and have a certain length gap.
+  */
+function createPattern(startsWith, endsWith, gapLength){
+  startPattern = '^' + startsWith;
+  endPattern = endsWith + '$';
 
-    //console.log('Now to get words')
-    //var data = httpGet("http://www.greenworm.net/sites/default/files/gw-assets/enable1-wwf-v4.0-wordlist.txt");
-    //document.getElementById("words").innerHTML = data;
+  if (gapLength != ''){
+    console.log('BBB')
+    midPattern = '.{' + gapLength + ',' + gapLength + '}';
+  }
+  else {
+    midPattern = '.+';
+  }
+
+  var re = new RegExp(startPattern + midPattern + endPattern);
+  return re;
+}
+
+function checkWord(pattern, word){
+  var res = pattern.exec(word);
+  if (res == null){
+    return false;
+  }
+  else{
+    return res[0];
+  }
 }
