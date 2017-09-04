9d7ee55d89e61edf2c961f7a4b1c55dc747760ce
davidruffner <davidruffner@gmail.com>
2017-09-04 12:33:32 -0400

    Add input buttons for scrabble letters.

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-document.write("Chapter 2 Exercise 3")
-
-
-for (var number = 1; number <= 100; number += 1){
-    if ((number % 5 != 0) && (number % 3 != 0)){
-	document.write(number)
-    }else if((number % 5 != 0) && (number % 3 == 0)){
-	document.write("Fizz")
-    }else if((number % 5 == 0) && (number % 3 != 0)){
-	document.write("Buzz")
-    }
-    else if((number % 5 == 0) && (number % 3 == 0)){
-	document.write("FizzBuzz")
-    }
+function myFunction() {
+    var letters = document.getElementById("letters").value;
+    var startsWith = document.getElementById("startsWith").value;
+    var endsWith = document.getElementById("endsWith").value;
+    var gapLength = document.getElementById("gapLength").value;
+    document.getElementById("demo").innerHTML = letters + startsWith + endsWith + gapLength;
 }
